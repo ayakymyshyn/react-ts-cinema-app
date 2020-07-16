c82b62871ed0390f254534949cf937ed9e394d56
Yakymyshyn <aiyn@gft.com>
2020-07-16 16:58:48 +0200

    small fix

--- src/api/fetchMovies.ts
+++ src/api/fetchMovies.ts
@@ -1,6 +1,6 @@
 import { Movie } from '../types/movieTypes';
 
-export let fetchMovies = async (): Promise<Array<Movie>> => {
+export const fetchMovies = async (): Promise<Array<Movie>> => {
   const data = await fetch('https://afternoon-woodland-86059.herokuapp.com/movies');
   const movies = await data.json();
   return movies;
